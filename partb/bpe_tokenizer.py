9a8d792dc02a62c6d7fc6ddf79596fa4655b44ac
bansal-yash <bansalyash54321@gmail.com>
2026-02-19 00:43:41 +0530

    trivial tokenizer

--- partb/bpe_tokenizer.py
+++ partb/bpe_tokenizer.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 class BPETokenizer:
     def __init__(self, vocab_size, special_tokens=None):
         self.vocab_size = vocab_size
@@ -9,24 +12,49 @@ class BPETokenizer:
         self.token_to_id = {}
         self.id_to_token = {}
 
-        for i in range(len(special_tokens)):
-            self.token_to_id[special_tokens[i]] = i
-            self.id_to_token[i] = special_tokens[i]
+        for id, token in enumerate(special_tokens):
+            self.token_to_id[token] = id
+            self.id_to_token[id] = token
 
-        print(self.token_to_id)
-        print(self.id_to_token)
+    def cons_full_vocab(self, corpus):
+        curr_id = len(self.token_to_id)
+
+        for sentence in corpus:
+            for char in sentence:
+                if char not in self.token_to_id:
+                    self.token_to_id[char] = curr_id
+                    self.id_to_token[curr_id] = char
+                    curr_id += 1
 
     def train(self, corpus):
 
-        return
+        # Construct the full vocabulary from the corpus
+        self.cons_full_vocab(corpus)
+
+        pprint(self.token_to_id)
+        pprint(self.id_to_token)
 
     def encode(self, text):
+        tokens = [self.token_to_id["<|SOS|>"]]
 
-        return
+        for char in text:
+            if char in self.token_to_id:
+                tokens.append(self.token_to_id[char])
+            else:
+                tokens.append(self.get_unk_id())
+
+        tokens.append(self.token_to_id["<|EOS|>"])
+        return tokens
 
     def decode(self, token_ids):
+        tokens = []
+        for id in token_ids:
+            token = self.id_to_token[id]
 
-        return
+            if token not in self.special_tokens:
+                tokens.append(token)
+
+        return "".join(tokens)
 
     def save(self, filepath):
 
@@ -37,9 +65,7 @@ class BPETokenizer:
         return
 
     def get_vocab_size(self):
-
-        return
+        return len(self.token_to_id)
 
     def get_unk_id(self):
-
-        return
+        return self.token_to_id["<|UNK|>"]
